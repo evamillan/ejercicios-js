3b453f56875d6670bb5c09e5503653d60b605317
evamillan <evamillanmartin@gmail.com>
2017-04-23 19:50:53 +0200

    Added card suits
    Program now prints heart cards that are divisible by 3 (doesn't print diamonds for some reason)

--- js modulo 2/ejercicio3.js
+++ js modulo 2/ejercicio3.js
@@ -1,4 +1,22 @@
-for (var carta = 1; carta <= 12; carta = carta +1) {
+/*for (var carta = 1; carta <= 12; carta = carta +1) {
 if (carta % 3 == 0) {
 console.log(carta)
-  }  }
+  }  }*/
+
+for (var number = 1; number <= 12; number = number + 1){
+  suit = "c";
+for (var number = 1; number <= 12; number = number + 1){
+  suit = "d";
+for (var number = 1; number <= 12; number = number + 1){
+  suit = "p";
+for (var number = 1; number <= 12; number = number + 1){
+  suit = "t";
+if ((number % 3 == 0) && (suit = "c")) {
+  var card = number + suit;
+  console.log(card);
+}
+else if ((number % 3 == 0) && (suit = "d")) {
+  var card = number + suit;
+  console.log(card);
+}
+}}}}
